9e68fac517802e007420d35d00ea93bc22bd7551
sidpreneur <sidpreneur@gmail.com>
2026-02-09 16:37:23 +0530

    Initial Commit

---
+++ Still/StillApp.swift
@@ -0,0 +1,17 @@
+//
+//  StillApp.swift
+//  Still
+//
+//  Created by Siddhant Singh on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct StillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
